876bb628062ced355a2becb038a40495b1210009
magalifabri <magali_90@live.nl>
2022-11-13 18:26:43 +0100

    Make stopwatch count down

--- components/Quiz.tsx
+++ components/Quiz.tsx
@@ -1,6 +1,7 @@
 import styles from "../styles/Quiz.module.scss";
 import IQuizData from "../interfaces/IQuizData";
 import { GamePhase } from "../pages/index";
+import { useEffect, useState } from "react";
 
 type AppProps = {
     quizData: IQuizData;
@@ -8,6 +9,37 @@ type AppProps = {
 };
 
 export default function Quiz({ quizData, gamePhase }: AppProps) {
+    //#region VARIABLES
+
+    const [secondsRemaining, setSecondsRemaining] = useState(
+        quizData.time_limit_s
+    );
+    const [intervalId, setIntervalId] =
+        useState<ReturnType<typeof setInterval>>();
+    //#endregion
+
+    //#region USE EFFECTS
+
+    // count down selection phase time remaining
+    useEffect(() => {
+        if (gamePhase === GamePhase.SELECTION) {
+            const interval = setInterval(() => {
+                setSecondsRemaining((a) => a - 1);
+            }, 1000);
+
+            setIntervalId(interval);
+        }
+    }, [gamePhase]);
+
+    // trigger end of selection phase
+    useEffect(() => {
+        if (!secondsRemaining) {
+            clearInterval(intervalId);
+            console.log("time's up!");
+        }
+    }, [secondsRemaining]);
+    //#endregion
+
     return (
         <div className={styles.container}>
             <div className={styles.innerContainer}>
@@ -25,7 +57,10 @@ export default function Quiz({ quizData, gamePhase }: AppProps) {
                                 {/* <!-- Font Awesome Pro 5.15.4 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) --> */}
                                 <path d="M432 304c0 114.9-93.1 208-208 208S16 418.9 16 304c0-104 76.3-190.2 176-205.5V64h-28c-6.6 0-12-5.4-12-12V12c0-6.6 5.4-12 12-12h120c6.6 0 12 5.4 12 12v40c0 6.6-5.4 12-12 12h-28v34.5c37.5 5.8 71.7 21.6 99.7 44.6l27.5-27.5c4.7-4.7 12.3-4.7 17 0l28.3 28.3c4.7 4.7 4.7 12.3 0 17l-29.4 29.4-.6.6C419.7 223.3 432 262.2 432 304zm-176 36V188.5c0-6.6-5.4-12-12-12h-40c-6.6 0-12 5.4-12 12V340c0 6.6 5.4 12 12 12h40c6.6 0 12-5.4 12-12z" />
                             </svg>
-                            0:20
+                            0:
+                            {secondsRemaining >= 10
+                                ? secondsRemaining
+                                : "0" + secondsRemaining}
                         </div>
                     )}
                 </div>
